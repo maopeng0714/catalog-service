eb2261a8f0dd3320becfa82855f06706198828c9
Peng Mao <pmao@redhat.com>
2014-07-28 17:00:33 +0800

    Add the project skeletion

---
+++ core/src/main/java/com/redhat/service/impl/LookupManagerImpl.java
@@ -0,0 +1,37 @@
+package com.redhat.service.impl;
+
+import com.redhat.dao.LookupDao;
+import com.redhat.model.LabelValue;
+import com.redhat.model.Role;
+import com.redhat.service.LookupManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * Implementation of LookupManager interface to talk to the persistence layer.
+ *
+ * @author <a href="mailto:matt@raibledesigns.com">Matt Raible</a>
+ */
+@Service("lookupManager")
+public class LookupManagerImpl implements LookupManager {
+    @Autowired
+    LookupDao dao;
+
+    /**
+     * {@inheritDoc}
+     */
+    public List<LabelValue> getAllRoles() {
+        List<Role> roles = dao.getRoles();
+        List<LabelValue> list = new ArrayList<LabelValue>();
+
+        for (Role role1 : roles) {
+            list.add(new LabelValue(role1.getName(), role1.getName()));
+        }
+
+        return list;
+    }
+}
---
+++ core/src/main/java/com/redhat/service/impl/PasswordTokenManager.java
@@ -0,0 +1,19 @@
+package com.redhat.service.impl;
+
+import com.redhat.model.User;
+
+
+public interface PasswordTokenManager {
+
+    /**
+     * {@inheritDoc}
+     */
+    String generateRecoveryToken(User user);
+
+    /**
+     * {@inheritDoc}
+     */
+    boolean isRecoveryTokenValid(User user, String token);
+
+    void invalidateRecoveryToken(User user, String token);
+}
